e70fe8f948747615660d82236fb7132a5c83c3e0
chengdu <3281328128@qq.com>
2021-05-24 21:51:45 +0800

    update pom

--- src/main/java/com/scd/mvctest/business/controller/FileController.java
+++ src/main/java/com/scd/mvctest/business/controller/FileController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -22,7 +23,8 @@ public class FileController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
 
     @PostMapping("/upload")
-    public String upload(@RequestPart(value = "business") MultipartFile multipartFile,
+    public String upload(HttpServletRequest httpServletRequest,
+                         @RequestPart(value = "business") MultipartFile multipartFile,
                          @RequestParam(value = "sequence") Integer sequence) throws IOException {
         String fileName = multipartFile.getOriginalFilename();
         byte[] bytes = multipartFile.getBytes();
